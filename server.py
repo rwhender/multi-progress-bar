6c5e430a33558aa2cde5ef441d6eba2b1fefc2db
Wesley Henderson <wesley.henderson11@gmail.com>
2022-10-22 21:27:32 -0500

    Initial commit

---
+++ server.py
@@ -0,0 +1,62 @@
+"""
+Module and script providing the multi-progress bar http server.
+
+Adapted from https://gist.github.com/nitaku/10d0662536f37a087e1b
+"""
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import cgi
+
+
+class ProgressBarServer(BaseHTTPRequestHandler):
+
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    # GET sends back a Hello world message
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write(json.dumps({'hello': 'world', 'received': 'ok'}).encode("utf-8"))
+
+    # POST echoes the message adding a JSON field
+    def do_POST(self):
+        ctype, _pdict = cgi.parse_header(self.headers.get('content-type'))
+
+        # refuse to receive non-json content
+        if ctype != 'application/json':
+            self.send_response(400)
+            self.end_headers()
+            return
+
+        # read the message and convert it into a python dictionary
+        length = int(self.headers.get('content-length'))
+        message = json.loads(self.rfile.read(length))
+
+        # add a property to the object, just to mess with data
+        message['received'] = 'ok'
+
+        # send the message back
+        self._set_headers()
+        self.wfile.write(json.dumps(message).encode("utf-8"))
+
+
+def run(server_class=HTTPServer, handler_class=ProgressBarServer, port=8008):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+
+    print('Starting httpd on port %d...' % port)
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
