9f15e8a0d84c546f22d4c16838d9516e09f2ef70
Wesley Henderson <wesley.henderson11@gmail.com>
2022-10-23 17:06:28 -0500

    Fleshed out message data structure a bit

--- multi_progress_bar/data.py
+++ multi_progress_bar/data.py
@@ -2,3 +2,36 @@
 Module defining data structures used by the package.
 """
 from dataclasses import dataclass
+from typing import List, Optional
+
+
+@dataclass
+class ProgressBarUpdate:
+    """
+    Data class defining messages containing updates for client progress.
+    """
+
+    name: str
+    """
+    The name of the progress bar this update pertains to. This should be unique.
+    """
+    current_value: int
+    """
+    The integer value to update the progress bar to.
+    """
+    parent: Optional[str] = None
+    """
+    The parent of this progress bar. Used for cases of nested progress bars.
+    """
+    total: Optional[int] = None
+    """
+    The expected final, "full" value for this progress bar.
+    """
+    description: Optional[str] = None
+    """
+    The description to display for this progress bar.
+    """
+
+
+# Type aliases
+ProgressBarUpdates = List[ProgressBarUpdate]
